b3f43eac34c0541faa2a8bf7cbd0f3fb6f2b3424
山下拓真 <yamashitatakushin@MacBook-Pro.local>
2021-09-05 20:38:06 +0900

    networt interface has ip address

---
+++ net.c
@@ -0,0 +1,148 @@
+#include "graph.h"
+#include "utils.h"
+#include <memory.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static unsigned int hash_code(void *ptr, unsigned int size){
+    
+    unsigned int value = 0, i = 0;
+    char *str = (char *)ptr;
+
+    while(i < size){
+	/* 1byteずつ計算する */
+	value += *str;
+	value *= 97;
+	str++;
+	i++;
+    }
+
+    return value;
+}
+
+/* interfaceに一意なMACアドレスを割り振ります */
+void interface_assign_mac_address(interface_t *interface){
+    
+    node_t *node = interface->att_node;
+
+    if(!node)
+	return;
+
+    unsigned int hash_code_val = 0;
+
+    /* ノードの名前と、インターフェースの名前を組み合わせて一意にする */
+    hash_code_val = hash_code(node->node_name, NODE_NAME_SIZE);
+    hash_code_val *= hash_code(interface->if_name, IF_NAME_SIZE);
+
+    /* アドレスをセット */
+    memset(IF_MAC(interface), 0, sizeof(IF_MAC(interface)));
+    memcpy(IF_MAC(interface), (char *)&hash_code_val, sizeof(unsigned int));
+}
+
+/* ノードにループバックアドレスをセットします */
+bool_t node_set_loopback_address(node_t *node, char *ip_addr){
+    
+    assert(ip_addr);
+
+    /* nodeのループバックアドレスにセット */
+    strncpy(NODE_LO_ADDR(node), ip_addr, 16);
+    NODE_LO_ADDR(node)[15] = '\0';
+    /* 設定されたのでtrue */
+    node->node_nw_prop.is_lb_addr_config = TRUE;
+
+    return TRUE;
+}
+
+/* ノードの特定のインターフェースにipアドレスをセットします */
+bool_t node_set_intf_ip_address(node_t *node, char *local_if, char *ip_addr, char mask){
+    
+    interface_t *interface = get_node_if_by_name(node, local_if);
+
+    if(!interface)
+	assert(0);
+
+    /* アドレスをセット */
+    strncpy(IF_IP(interface), ip_addr, 16);
+    IF_IP(interface)[15] = '\0';
+    
+    /* マスクをセット */
+    interface->intf_nw_props.mask = mask;
+
+    /* ipアドレスをセットしたのでtrueにする */
+    interface->intf_nw_props.is_ipadd_config = TRUE;
+
+    return TRUE;
+}
+
+bool_t node_unset_intf_ip_address(node_t *node, char *local_if){
+    
+    return TRUE;
+}
+
+/* ノードのネットワークプロパティーをダンプします */
+void dump_node_nw_props(node_t *node){
+    
+    printf("\nNode Name = %s\n", node->node_name);
+
+    if(node->node_nw_prop.is_lb_addr_config){
+	
+	printf("\t  lo addr : %s/32", NODE_LO_ADDR(node));
+    }
+
+    printf("\n");
+}
+
+/* インターフェースのネットワークプロパティをダンプします */
+void dump_intf_props(interface_t *interface){
+    
+    dump_interface(interface);
+
+    if(interface->intf_nw_props.is_ipadd_config){
+	
+	printf("\t IP Addr = %s/%u", IF_IP(interface), interface->intf_nw_props.mask);
+	printf(
+	    "\t MAC : %u:%u:%u:%u:%u:%u\n",
+	    IF_MAC(interface)[0],
+	    IF_MAC(interface)[1],
+	    IF_MAC(interface)[2],
+	    IF_MAC(interface)[3],
+	    IF_MAC(interface)[4],
+	    IF_MAC(interface)[5]    
+	);
+    } else {
+	
+	
+    }
+}
+
+
+void dump_nw_graph(graph_t *graph){
+    
+    node_t *node;
+    glthread_t *curr;
+    interface_t *interface;
+    unsigned int i;
+
+    printf("Topology Name = %s\n", graph->topology_name);
+
+    ITERATE_GLTHREAD_BEGIN(&graph->node_list, curr){
+
+	node = graph_glue_to_node(curr);
+	
+	/* nodeのpropをダンプ */
+	dump_node_nw_props(node);
+
+	for(i = 0; i < MAX_INTF_PER_NODE; i++){
+	    
+	    interface = node->intf[i];
+
+	    if(!interface)
+		break;
+	    
+	    /* interfaceのpropsをダンプ */
+	    dump_intf_props(interface);
+	}
+
+    }ITERATE_GLTHREAD_END(&graph->node_list, curr);
+}
+
--- net.h
+++ net.h
@@ -1,7 +1,8 @@
 #ifndef __NET__
 #define __NET__
 
-//#include "utils.h"
+#include "utils.h"
+#include "graph.h"
 #include <memory.h>
 
 typedef struct graph_ graph_t;
@@ -36,7 +37,7 @@ typedef struct ip_add_ {
  * 0xC81F66243AC8
  */
 typedef struct mac_add_ {
-    unsigned char mac[8];
+    unsigned char mac[6];
 } mac_add_t;
 
 //#pragma pack(pop)
@@ -45,13 +46,13 @@ typedef struct mac_add_ {
 typedef struct node_nw_prop_ {   
     /* loop back かどうか */
     bool_t is_lb_addr_config;
-    ip_addr_t lb_addr;
+    ip_add_t lb_addr;
 } node_nw_prop_t;
 
 /* nodeのネットワークプロパティの初期化 */
 static inline void init_node_nw_prop(node_nw_prop_t *node_nw_prop){
     
-    node_nw_prop->is_lb_addr_config = false;
+    node_nw_prop->is_lb_addr_config = FALSE;
 
     memset(node_nw_prop->lb_addr.ip_addr, 0, 16);
 }
@@ -69,7 +70,7 @@ typedef struct intf_nw_props {
 static inline void init_intf_nw_prop(intf_nw_props_t *intf_nw_props){
     
     memset(intf_nw_props->mac_add.mac, 0, 8);
-    intf_nw_props->is_is_ipadd_config = false;
+    intf_nw_props->is_ipadd_config = FALSE;
     memset(intf_nw_props->ip_add.ip_addr, 0, 16);
     intf_nw_props->mask = 0;
 }
@@ -82,6 +83,10 @@ static inline void init_intf_nw_prop(intf_nw_props_t *intf_nw_props){
 bool_t node_set_loopback_address(node_t *node, char *ip_addr);
 bool_t node_set_intf_ip_address(node_t *node, char *local_if, char *ip_addr, char mask);
 bool_t node_unset_intf_ip_address(node_t *node, char *local_if);
+
+void dump_nw_graph(graph_t *graph);
+void dump_node_nw_props(node_t *node);
+void dump_intf_props(interface_t *interface);
 
 #endif
 
